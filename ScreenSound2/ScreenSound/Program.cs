7c80781d4ff47b6e7ef285ecc865e744bb0ac717
Milena Araújo Lima <127094856+MilenaALima@users.noreply.github.com>
2024-03-24 17:39:46 -0300

    Add files via upload

---
+++ ScreenSound2/ScreenSound/Program.cs
@@ -0,0 +1,13 @@
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("João");
+
+Episodio ep2 = new(2, "Técnicas de aprendizado", 57);
+ep2.AdicionarConvidados("Rafael");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flávia");
+
+Podcast podcast = new("Podcast Especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
